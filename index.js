e1f89f6c294af3fb03c76402983f9b668c74f053
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-18 19:27:05 +0300

    completed phase-0 intro to js arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    name = cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    name = cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = [...cats,`${name}`];
+    return newCats;
+}
+
+console.log(appendCat('Broom'));
+
+function prependCat(name) {
+    const newCats = [`${name}`,...cats];
+    return newCats;
+}
+
+console.log(prependCat('Arnold'));
+
+function removeLastCat() {
+    const newCats = cats.slice(0,2);
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
+
+
+
